a199c9e1345c46dc02568c8c9f1949f73a5c8d0e
5arSanti <santiari05@hotmail.com>
2023-07-18 14:09:01 -0500

    Change in project files and dependencies.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+// import { WebView} from "react-native-webview";
 
 //Componentes
 import { Welcome } from "./Pages/components/Welcome";
@@ -8,13 +9,16 @@ export default function App() {
     const names = ["Santiago", "Sebastian", "Laura"];
 
     return(
+        // <WebView source={{uri: "https://5arsanti.github.io/"}}/>
         <View style={styles.container}>
             <StatusBar style="auto"/>
 
             {names.map((item) => (
-                <Welcome name={item}/>
+                <Welcome 
+                    key={item}
+                    name={item}
+                />
             ))}
-            
         </View>
     );
 
