f024361fb4e8a34487042467488a5ee8d1d1e4d4
5arSanti <santiari05@hotmail.com>
2023-07-18 13:11:04 -0500

    The initial application was created with basic concepts and other things.

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+//Componentes
+import { Welcome } from "./Pages/components/Welcome";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const names = ["Santiago", "Sebastian", "Laura"];
+
+    return(
+        <View style={styles.container}>
+            <StatusBar style="auto"/>
+
+            {names.map((item) => (
+                <Welcome name={item}/>
+            ))}
+            
+        </View>
+    );
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+})
